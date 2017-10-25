b60d77df8f9b7326a0db3a6fc9060ce8337b2bba
Steven Haryanto (on pc-office) <steven@masterweb.com>
2017-10-25 22:41:22 +0700

    Finish first version

--- lib/App/WHMCSUtils.pm
+++ lib/App/WHMCSUtils.pm
@@ -1,4 +1,4 @@
-package App::WHMCSUtils;
+ package App::WHMCSUtils;
 
 # AUTHORITY
 # DATE
@@ -47,11 +47,26 @@ _
         client_email => {
             schema => 'str*',
         },
+        client_id => {
+            schema => 'posint*',
+        },
+        restore_invoices => {
+            schema => 'bool*',
+            default => 1,
+        },
+        restore_hostings => {
+            schema => 'bool*',
+            default => 1,
+        },
+        restore_domains => {
+            schema => 'bool*',
+            default => 1,
+        },
     },
     args_rels => {
         'req_one&' => [
             ['sql_backup_file', 'sql_backup_dir'],
-            ['client_email'],
+            ['client_email', 'client_id'],
         ],
     },
     deps => {
@@ -92,9 +107,9 @@ sub restore_whmcs_client {
             $CWD = $sql_backup_dir;
             my @cmd;
             if ($decompress) {
-                push @cmd, "zcat", $pt->cwd,, "|";
+                push @cmd, "zcat", $pt->absolute->stringify, "|";
             } else {
-                push @cmd, "cat", $pt->cwd, "|";
+                push @cmd, "cat", $pt->absolute->stringify, "|";
             }
             push @cmd, "mysql-sql-dump-extract-tables",
                 "--include-table-pattern", '^(tblclients|tblinvoices|tblinvoiceitems|tblorders)$';
@@ -109,24 +124,103 @@ sub restore_whmcs_client {
 
     my @sql;
 
-    my ($clientid);
+    my $clientid = $args{client_id};
   FIND_CLIENT:
     {
         open my $fh, "<", "tblclients"
             or return [500, "Can't open $sql_backup_dir/tblclients: $!"];
-        my $clientemail = lc $args{client_email};
+        my $clientemail;
+        $clientemail = lc $args{client_email} if defined $args{client_email};
         while (<$fh>) {
             next unless /^INSERT INTO `tblclients` \(`id`, `firstname`, `lastname`, `companyname`, `email`, [^)]+\) VALUES \((\d+),'(.*?)','(.*?)','(.*?)','(.*?)',/;
             my ($rid, $rfirstname, $rlastname, $rcompanyname, $remail) = ($1, $2, $3, $4, $5);
-            if (lc $remail eq $clientemail) {
-                $clientid = $rid;
-                push @sql, $_;
-                log_info "Found client email=%s in backup: ID=%s", $clientemail, $clientid;
-                last FIND_CLIENT;
+            if (defined $clientid) {
+                # find by ID
+                if ($rid == $clientid) {
+                    $clientemail = $remail;
+                    push @sql, $_;
+                    log_info "Found client ID=%s in backup", $clientid;
+                    last FIND_CLIENT;
+                }
+            } else {
+                # find by email
+                if (lc $remail eq $clientemail) {
+                    $clientid = $rid;
+                    push @sql, $_;
+                    log_info "Found client email=%s in backup: ID=%s", $clientemail, $clientid;
+                    last FIND_CLIENT;
+                }
             }
         }
-        return [404, "Couldn't find client email=%s in database backup, please check the email or try another backup"];
+        return [404, "Couldn't find client email=$clientemail in database backup, please check the email or try another backup"];
     }
+
+    my @invoiceids;
+  FIND_INVOICES:
+    {
+        last unless $args{restore_invoices};
+        open my $fh, "<", "tblinvoices"
+            or return [500, "Can't open $sql_backup_dir/tblinvoices: $!"];
+        while (<$fh>) {
+            next unless /^INSERT INTO `tblinvoices` \(`id`, `userid`, [^)]+\) VALUES \((\d+),(\d+),/;
+            my ($rid, $ruserid) = ($1, $2);
+            if ($ruserid == $clientid) {
+                push @invoiceids, $rid;
+                push @sql, $_;
+                log_info "Found client invoice in backup: ID=%s", $rid;
+            }
+        }
+        log_info "Number of invoices found for client in backup: %d", ~~@invoiceids if @invoiceids;
+    }
+
+  FIND_INVOICEITEMS:
+    {
+        last unless @invoiceids;
+        open my $fh, "<", "tblinvoiceitems"
+            or return [500, "Can't open $sql_backup_dir/tblinvoiceitems: $!"];
+        while (<$fh>) {
+            next unless /^INSERT INTO `tblinvoiceitems` \(`id`, `invoiceid`, `userid`, [^)]+\) VALUES \((\d+),(\d+),(\d+)/;
+            my ($rid, $rinvoiceid, $ruserid) = ($1, $2, $3);
+            if (grep {$rinvoiceid == $_} @invoiceids) {
+                log_trace "Adding invoice item %s for invoice #%s", $rid, $rinvoiceid;
+                push @sql, $_;
+            }
+        }
+    }
+
+  FIND_HOSTINGS:
+    {
+        last unless $args{restore_hostings};
+        open my $fh, "<", "tblhosting"
+            or return [500, "Can't open $sql_backup_dir/tblhosting: $!"];
+        while (<$fh>) {
+            next unless /^INSERT INTO `tblhosting` \(`id`, `userid`, [^)]+\) VALUES \((\d+),(\d+),(\d+)/;
+            my ($rid, $ruserid) = ($1, $2, $3);
+            if ($ruserid == $clientid) {
+                log_trace "Found hosting for client in backup: ID=%d", $rid;
+                push @sql, $_;
+            }
+        }
+    }
+
+  FIND_DOMAINS:
+    {
+        last unless $args{restore_domains};
+        open my $fh, "<", "tbldomains"
+            or return [500, "Can't open $sql_backup_dir/tbldomains: $!"];
+        while (<$fh>) {
+            next unless /^INSERT INTO `tbldomains` \(`id`, `userid`, [^)]+\) VALUES \((\d+),(\d+),(\d+)/;
+            my ($rid, $ruserid) = ($1, $2, $3);
+            if ($ruserid == $clientid) {
+                log_trace "Found domain for client in backup: ID=%d", $rid;
+                push @sql, $_;
+            }
+        }
+    }
+
+    # TODO: tickets?
+
+    # records in tblaccounts (transactions) are not deleted when client is deleted
 
     [200, "OK", \@sql];
 }
